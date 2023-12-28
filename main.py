a549c277539ae6341a48669eaa6bb645d8492df9
helloworldgod <64147005+wtsgold@users.noreply.github.com>
2023-12-27 22:06:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,58 @@ def start(message):
     except Exception as e:
         print(str(e))
 
-# 其他处理函数...
+def blocked(message):
+    try:
+        if message.chat.id == main_id:
+            get = database_query("SELECT user_id FROM USERS WHERE messageid = ?", (message.reply_to_message.message_id,))
+            for i in get:
+                bot.send_message(i[0], ban_msg)
+                database_query("INSERT INTO blocked VALUES(?)", (i[0],))
+    except Exception as e:
+        print(str(e))
+
+def unblocked(message):
+    try:
+        if message.chat.id == main_id:
+            get = database_query("SELECT user_id FROM blocked WHERE user_id = ?", (message.from_user.id,))
+            if get:
+                database_query("DELETE FROM blocked WHERE user_id = ?", (message.from_user.id,))
+                bot.send_message(message.from_user.id, unban_msg)
+    except Exception as e:
+        print(str(e))
+
+def message_everyone(message):
+    usrs = database_query("SELECT user_id FROM user")
+    for i in usrs:
+        try:
+            bot.copy_message(i[0], message.chat.id, message.message_id)
+        except Exception as e:
+            print("Error sending message: ", str(e))
+
+def other(message):
+    try:
+        if message.chat.id != main_id:
+            if message.forward_from is None:
+                get = database_query("SELECT user_id FROM blocked WHERE user_id = ?", (message.from_user.id,))
+                if get != []:
+                    bot.send_message(message.chat.id, banned_msg)
+                else:
+                    # 处理其他类型的消息
+                    pass
+    except Exception as e:
+        print(str(e))
+
+def text(message):
+    try:
+        get = database_query("SELECT user_id FROM blocked WHERE user_id = ?", (message.from_user.id,))
+        if get != []:
+            bot.send_message(message.chat.id, banned_msg)
+        else:
+            if message.chat.id != main_id:
+                # 处理文本消息
+                pass
+    except Exception as e:
+        print(str(e))
 
 # 设置 webhook 路由
 @app.route('/' + TOKEN, methods=['POST'])
@@ -60,9 +111,22 @@ def receive_update():
 def start_handler(message):
     start(message)
 
-# 其他 bot.message_handler 装饰器
+@bot.message_handler(commands=['ban'])
+def block_handler(message):
+    blocked(message)
+
+@bot.message_handler(commands=['unban'])
+def unblock_handler(message):
+    unblocked(message)
+
+@bot.message_handler(commands=['admin_message'])
+def message_everyone_handler(message):
+    message_everyone(message)
+
+# 添加其他 bot.message_handler 装饰器
 
 # 启动 Flask 应用
 if __name__ == "__main__":
     create_db_new()  # 创建数据库（如果尚未创建）
     app.run(debug=True)
+
